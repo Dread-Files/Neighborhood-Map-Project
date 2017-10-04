94b1fa15c27640befc6a3a378b24dc38defdc8b9
Asad Ibrahim <fahadi1997@gmail.com>
2017-10-04 17:16:47 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var map,
     infoWindow,
     markers = [];
 
-
+// my locations arrey to add markers and knockout
 var locations = [
     {title: "Six flags America", location:{lat: 38.907313, lng: -76.774371}},
     {title: "Mount Vernon, VA", location:{lat: 38.707961, lng: -77.086521}},
@@ -10,19 +10,23 @@ var locations = [
     {title: "Burke Lake Park", location:{lat :38.760764, lng: -77.307478}},
     {title: "Ocean City, VA", location:{lat: 38.391591, lng: -75.064601}}
 ];
-//initalizes the map.
+
+// initalizes the map.
 function initMap() {
-  //embeds the map in an html element with the id = map
+
+  // loads the map in an html element with an id = map
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10, center:{lat: 38.907313, lng: -76.774371}
   });
 
   infoWindow = new google.maps.InfoWindow();
-  // create the infoWindow for each item in the locations arrey.
+
+  // iterates through the locations arrey to create markers and infoWindow
   for (var i = 0; i < locations.length; i++) {
     createMarkersForPlaces();
   }
 
+// creates markers for each locations item
   function createMarkersForPlaces() {
     var position = locations[i].location;
     var title = locations[i].title;
@@ -36,12 +40,21 @@ function initMap() {
       zoom: 13
     });
 
+// opens the infoWindow for a marker when it is selected on the map
     marker.addListener('click', function() {
       populateInfoWindow(this, infoWindow);
     });
 
-    markers.push(marker);
+// passes each marker back to the locations object arrey to be used in knockout
+    locations[i].markers = marker;
   }
+
+// the knockout viewModel had to be called here after the markers were made to avoid
+// errors when they were selected from the side panel
+  var viewModel = new ViewModel();
+
+  ko.applyBindings(viewModel);
+
   // this function adds content to the infoWindow when a marker is selected.
   function populateInfoWindow(marker, infoWindow) {
     if (infoWindow.marker != marker) {
@@ -50,7 +63,8 @@ function initMap() {
       infoWindow.setContent("<div><h2>" + marker.title + "</h2></div>" + "<div id=\"wiki-container\">" +
           "<h3 id=\"wikipedia-header\">Relevant Wikipedia Links</h3>" +
           "</div>");
-      // this section uses  wiki api to add links for more relevent information.
+
+// wiki api adds links for more relevent information to their apropriate infoWindow.
       var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '&format=json&callback=wikiCallback';
       $.ajax({
         url: wikiUrl,
@@ -78,39 +92,32 @@ function initMap() {
 
 // this section utilizes the knockout library.
 var ViewModel = function() {
-  // here the elements in locations arrey is passed to listMarkers arrey and then displayed in a list, in the side panel.
-  this.listMarkers = ko.observableArray([]);
-  for(var i = 0; i < locations.length; i++) {
-    this.listMarkers.push(locations[i]);
-  }
-  // this function is suppose to filter throught the list when text is entered in the search bar, currently isn't working properly.
-  this.panToMap = ko.computed(function() {
-    this.data = document.getElementById("#selector");
-    console.log(this.data);
-    for(var i = 0; i < locations.length; i++) {
-      this.lat = ko.observable();
-      this.lng = ko.observable();
-      if (this.data === locations[i].title) {
-        this.lat = locations[i].location.lat;
-        this.lng = locations[i].location.lng;
-        google.maps.panTo(this.lat, this.lng);
-      }
-    }
-  });
+  var self = this;
 
+//initializes filter with a value to avoid error in filteredItems
+  this.filter = ko.observable('');
+
+// opens the infoWindow and centers on a marker when an item is selected from the side panel
+  this.panToMap = function(location) {
+
+// uses the location object in the locations arrey to center on a marker
+    map.panTo(location.location);
+    google.maps.event.trigger(location.markers, 'click');
+  }
+
+// knockout filter function to 'filter' items in the side panel
   this.filteredItems = ko.computed(function() {
-      this.filter = ko.observable();
-      if (!filter) {
-          return this.listMarkers;
-      } else {
-          return ko.utils.arrayFilter(this.listMarkers, function(item) {
-              return ko.utils.stringStartsWith(listMarkers.title.toLowerCase(), filter);
-          });
-      }
+
+// makes values in the search bar to lower case
+      var selector = self.filter().toLowerCase();
+      return ko.utils.arrayFilter(locations, function(item) {
+
+// the locations that do not return value of -1 when knockout is filtering and stores them in match
+         var match = item.title.toLowerCase().indexOf(selector) !== -1;
+
+// returns the filtered items to the side panel
+         item.markers.setVisible(match);
+         return match;
+      });
   });
 }
-
-
-var viewModel = new ViewModel();
-
-ko.applyBindings(viewModel);
